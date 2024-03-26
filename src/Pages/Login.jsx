14ef77aaaea35463ede0b78666e32f4ee091c89b
bechir-elya <bechir.elya@gmail.com>
2024-03-26 16:34:41 +0100

    editing login page and adding style

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,55 @@
-
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import gbg from '../assets/login-img/gbg.jpg'
 
 function Login() {
+
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [errorEmail, setErrorEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorPassword, setErrorPassword] = useState('');
+
+  function connectProfile() {
+    const data = JSON.parse(localStorage.getItem('userData'));
+
+    const userIndex = data.findIdex((item) => item.email === email);
+    if (userIndex == -1) {
+      setErrorEmail('User not found');
+    } else {
+      if (password == data[userIndex].password) {
+        navigate('/');
+      } else {
+        setErrorPassword('Incorrect password');
+      }
+    }
+  }
+
+
   return (
-    <div>Login</div>
+    <>
+      <section className="loginPage">
+        <Container>
+          <div className='mainDiv'>
+            <img src={gbg} alt="gaming bg" className='loginBG'/>
+            <div className='logFormular'>
+              <div className='d-flex align-items-baseline w-50' style={{ margin: 'auto' }}>
+                <Form.Control type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}  />
+                {<span>{errorEmail}</span>}
+              </div>
+              <div className='d-flex align-items-baseline w-50' style={{ margin: 'auto' }}>
+                <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                {<span>{errorPassword}</span>}
+              </div>
+              <Button variant="primary" className='d-block' style={{ margin: 'auto' }} onClick={connectProfile}>Log in</Button>
+            </div>
+          </div>
+        </Container>
+      </section>
+    </>
   )
 }
 
